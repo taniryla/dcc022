ffc80598220db56f4143bb4a2cc2ad83309f43f2
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 10:14:28 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 22-intersection
+Difficulty: Intermediate
+Prompt:
+- Write a function named intersection that accepts two arguments which are both arrays.  The array arguments may contain any mixture of strings, numbers and/or booleans - but no reference types, i.e., objects.
+- The function should return a new array containing all elements in common, including repeating element values.
+- The ordering of the elements in the returned is not important.
+- If there are no elements in the arrays in common,  the intersection function should return an empty array.
+- The function should not mutate (change) either argument.
+Examples:
+intersection(['a', 1], []) //=> []
+intersection(['a', 1], [true, 'a', 15]) //=> ['a']
+intersection([1, 'a', true, 1, 1], [true, 1, 'b', 1]) //=> [1, true, 1]
+-----------------------------------------------------------------*/
+// Your solution for 22-intersection here:
+
+
+</div>
+`;
